7494d5d965b47ed0dc30758da1b9a2378423b992
Björn Ramberg <bjornr@spotify.com>
2022-12-05 20:02:51 +0100

    Fix horrible decisions
    Using a list instead of tuple was a horrible idea. This way the code
    is much cleaner! (Thx dstaby)

--- src/main/scala/Day4.scala
+++ src/main/scala/Day4.scala
@@ -1,33 +1,22 @@
 object Day4 extends Shared {
 
-  implicit class SeqPair[T](pair: List[T]) {
-    def pairApply[U](fn: (a: T, b: T) => U): U = pair match {
-      case a :: b :: Nil => fn(a, b)
-      case _             => throw new Error(s"Gahhh! Not 2 elements: $pair")
-    }
-  }
+  val line = """(\d+)-(\d+),(\d+)-(\d+)""".r
 
   private def parseInput(input: String) = {
     input
       .split("\n")
-      .map(
-        _.split(',').toList
-          .map(_.split('-').map(_.toInt).toList)
-          .map(_.pairApply(Range.inclusive))
-      )
+      .map { case line(a, b, c, d) => (a.toInt to b.toInt, c.toInt to d.toInt) }
   }
 
   def ans(input: String) =
     parseInput(input)
-      .count(_.pairApply { (l, r) =>
-        (l containsSlice r) || (r containsSlice l)
-      })
+      .count { case (l, r) => (l containsSlice r) || (r containsSlice l) }
 
   def ans2(input: String) = parseInput(input)
-    .count(_.pairApply { (l, r) =>
+    .count { case (l, r) =>
       (l contains r.start) || (l contains r.last)
       || (r contains l.start) || (r contains l.last)
-    })
+    }
 
   println(ans(input))
   println(ans2(input))
