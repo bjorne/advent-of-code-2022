ed185a7268e13dbbbce10d549e6cc22bfedfc014
Björn Ramberg <bjornr@spotify.com>
2022-12-06 08:32:27 +0100

    Tweak

--- src/main/scala/Day6.scala
+++ src/main/scala/Day6.scala
@@ -4,10 +4,10 @@ import scala.util.Try
 object Day6 extends Shared {
 
   def ans(input: String) =
-    4 + input.sliding(4).indexWhere(_.toCharArray.toSet.size == 4)
+    4 + input.sliding(4).indexWhere(_.toSet.size == 4)
 
   def ans2(input: String) =
-    14 + input.sliding(14).indexWhere(_.toCharArray.toSet.size == 14)
+    14 + input.sliding(14).indexWhere(_.toSet.size == 14)
 
   println(ans(input))
   println(ans2(input))
