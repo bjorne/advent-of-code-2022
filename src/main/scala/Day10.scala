b17640ecc260abe670a900d8d186678c8135e887
Björn Ramberg <bjornr@spotify.com>
2022-12-10 14:33:54 +0100

    Day 10

---
+++ src/main/scala/Day10.scala
@@ -0,0 +1,44 @@
+import lib.Point
+
+object Day10 extends Shared {
+  val checkpoints = List(20, 60, 100, 140, 180, 220)
+  val width = 40
+  val height = 6
+
+  private def run(input: String) = {
+    input
+      .split("\n")
+      .flatMap {
+        case s"addx $value" => List(0, value.toInt)
+        case s"noop"        => List(0)
+      }
+      .scanLeft(1) { (instr, acc) => acc + instr }
+      .zipWithIndex
+  }
+
+  def ans(input: String) = run(input).collect {
+    case (v, index) if checkpoints.contains(index + 1) => v * (index + 1)
+  }.sum
+
+  def ans2(input: String) = {
+    val pixels = run(input)
+      .foldLeft(Set.empty[Point]) { (pixels, pair) =>
+        pair match {
+
+          case (value, index) =>
+            val add = ((value - 1) to (value + 1))
+              .find(_ == index % 40)
+              .map { v =>
+                val withOffset = (index % 40 - v) + index
+                Point(withOffset % 40, -withOffset / 40)
+              }
+              .toList
+            pixels ++ add
+        }
+      }
+    Point.drawPoints(pixels.map(p => p -> '#').toMap, padding = 0)
+  }
+
+  println(ans(input))
+  println(ans2(input))
+}
--- src/main/scala/lib/Point.scala
+++ src/main/scala/lib/Point.scala
@@ -13,18 +13,17 @@ case class Point(x: Int = 0, y: Int = 0) {
 }
 
 object Point {
-  def drawPoints(points: Map[Point, Char]): String = {
+  def drawPoints(points: Map[Point, Char], padding: Int = 1): String = {
     val min =
       points.keys.reduce((p1, p2) => Point(p1.x.min(p2.x), p1.y.min(p2.y)))
     val max =
       points.keys.reduce((p1, p2) => Point(p1.x.max(p2.x), p1.y.max(p2.y)))
-    println(s"$min $max")
     val chars = for {
-      x <- (min.x - 1) to (max.x + 1)
+      x <- (min.x - padding) to (max.x + padding)
 
     } yield {
       for {
-        y <- (min.y - 1) to (max.y + 1)
+        y <- (min.y - padding) to (max.y + padding)
         p = Point(x, y)
       } yield points.getOrElse(p, '.')
     }
